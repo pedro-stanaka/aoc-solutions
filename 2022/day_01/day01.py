307dcd190e3f63a3d52e50aa83148440dba9781c
Pedro Tanaka <pedro.tanaka@shopify.com>
2022-12-02 09:41:55 +0100

    2022 - Adding CI
    Signed-off-by: Pedro Tanaka <pedro.tanaka@shopify.com>

--- 2022/day_01/day01.py
+++ 2022/day_01/day01.py
@@ -13,12 +13,9 @@ def get_top_elf_calories(n: int, calories: list):
     return sum(heapq.nlargest(n, calories))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     input_data = get_data(day=1, year=2022, block=True)
     snack_calories = get_elves_snacks_calories(input_data)
 
     print("Part 2: {}".format(get_top_elf_calories(1, snack_calories)))
     print("Part 1: {}".format(get_top_elf_calories(3, snack_calories)))
-
-
-
