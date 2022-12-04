e22277b54994c21351a5c6f8cd600f63bd874cc6
Pedro Tanaka <pedro.tanaka@shopify.com>
2022-12-04 18:57:58 +0100

    2022 - forgotten bit of day 03
    Signed-off-by: Pedro Tanaka <pedro.tanaka@shopify.com>

--- 2022/day_03/solution.py
+++ 2022/day_03/solution.py
@@ -25,6 +25,15 @@ if __name__ == "__main__":
     input_data = get_data(day=3, year=2022, block=True)
 
     input_data = input_data.split("\n")
-    input_data = [(e[: len(e) // 2], e[len(e) // 2 :]) for e in input_data]
+    compartments = [(e[: len(e) // 2], e[len(e) // 2:]) for e in input_data]
 
-    print("Part 1: {}".format(calculate_priority(input_data)))
+    i = 0
+    badges_sum = 0
+    while i < len(input_data):
+        inter_group = set(input_data[i]).intersection(set(input_data[i + 1])).intersection(set(input_data[i + 2]))
+        if len(inter_group) == 1:
+            badges_sum += item_priority(inter_group.pop())
+        i += 3
+
+    print("Part 1: {}".format(calculate_priority(compartments)))
+    print("Part 2: {}".format(badges_sum))
